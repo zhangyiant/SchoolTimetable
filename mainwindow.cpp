42e500e24e0b85f9abd1bb6171f283d50842a06c
Yi Zhang <zhangyiant@hotmail.com>
2023-03-20 17:07:00 +0800

    ABC.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,12 @@
 #include "mainwindow.h"
 #include <QIcon>
+#include <QKeyEvent>
 #include <QDebug>
+#include <QDateTime>
+#include <QTimeZone>
+#include <QDate>
+
+#include <chrono>
 
 MainWindow::MainWindow()
 {
@@ -9,24 +15,54 @@ MainWindow::MainWindow()
 	this->trayIcon = new QSystemTrayIcon(this);
 	
 	// QIcon icon = QIcon("C:\\Users\\zhang\\git\\SchoolTimetable\\heart.png");
-	QIcon icon = QIcon("C:\\Users\\zhang\\git\\RegisterPrinter\\registerPrinter.png");
+	QIcon icon = QIcon("/Users/zhangpe/git/SchoolTimetable/heart.png");
 	this->trayIcon->setIcon(icon);
 	this->trayIconMenu = new QMenu(this);
 	this->trayIcon->setContextMenu(this->trayIconMenu);
-	this->min = new QAction("Minimize", this);
-	QAction *normal = new QAction("Normalize", this);
-	QAction* fullScreen = new QAction("FullScreen", this);
-	this->trayIconMenu->addAction(this->min);
-	this->trayIconMenu->addAction(normal);
-	this->trayIconMenu->addAction(fullScreen);
-	connect(min, &QAction::triggered, this, &MainWindow::hide);
-	connect(normal, &QAction::triggered, this, &MainWindow::showNormal);
-	connect(fullScreen, &QAction::triggered, this, &MainWindow::showFullScreen);
+	QAction* hideAction = new QAction("Hide", this);
+	QAction* normalAction = new QAction("Normalize", this);
+	QAction* fullScreenAction = new QAction("FullScreen", this);
+	QAction* quitAction = new QAction("Quit", this);
+	this->trayIconMenu->addAction(hideAction);
+	this->trayIconMenu->addAction(normalAction);
+	this->trayIconMenu->addAction(fullScreenAction);
+	this->trayIconMenu->addSeparator();
+	this->trayIconMenu->addAction(quitAction);
+	connect(hideAction, &QAction::triggered, this, &MainWindow::hide);
+	connect(normalAction, &QAction::triggered, this, &MainWindow::showNormal);
+	connect(fullScreenAction, &QAction::triggered, this, &MainWindow::showFullScreen);
+	connect(quitAction, &QAction::triggered, this, &MainWindow::close);
 	connect(trayIcon, &QSystemTrayIcon::activated, this, &MainWindow::iconActivated);
 	this->trayIcon->show();
 
 
+	this->expiredTime = QTime(16, 55);
+	QDateTime expiredDateTime = QDateTime(
+		QDate::currentDate(),
+		this->expiredTime,
+		QTimeZone::systemTimeZone()
+	);
+	QDateTime currentDateTime = QDateTime::currentDateTime();
 
+	qDebug() << expiredDateTime;
+	qDebug() << currentDateTime;
+
+	std::chrono::milliseconds t = expiredDateTime - currentDateTime;
+	qDebug() << t.count() << "seconds";
+	QTimer::singleShot(t, this, SLOT(timeout()));
+}
+
+void MainWindow::timeout() {
+	this->showFullScreen();
+	return;
+}
+
+void MainWindow::keyReleaseEvent(QKeyEvent *event) {
+	int key = event->key();
+	if (key == Qt::Key_Escape) {
+		this->showNormal();
+		this->hide();
+	}
 }
 
 void MainWindow::iconActivated(QSystemTrayIcon::ActivationReason reason)
--- mainwindow.h
+++ mainwindow.h
@@ -1,20 +1,27 @@
 #pragma once
-#include <QMainWindow>
+#include <QDialog>
 #include <QSystemTrayIcon>
 #include <QAction>
 #include <QMenu>
+#include <QTime>
+#include <QTimer>
 
 class MainWindow :
-    public QMainWindow
+    public QDialog
 {
     Q_OBJECT
 public:
     MainWindow();
+protected:
+    void keyReleaseEvent(QKeyEvent *event) override;
 private slots:
     void iconActivated(QSystemTrayIcon::ActivationReason reason);
+    void timeout();
 private:
     QSystemTrayIcon* trayIcon;
     QAction* min;
     QMenu* trayIconMenu;
+    QTime expiredTime;
+    QTimer* timer;
 };
 
