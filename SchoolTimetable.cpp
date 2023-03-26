a94095cb911b45d8e840c5750591ad655d4a5421
Yi Zhang <zhangyiant@hotmail.com>
2023-03-26 17:04:54 +0800

    Add first day data.

--- SchoolTimetable.cpp
+++ SchoolTimetable.cpp
@@ -3,5 +3,44 @@
 SchoolTimetable defaultSchoolTimetable;
 
 void initDefaultSchoolTimetable() {
+	std::list<SchoolClass> classList;
+	SchoolClass schoolClass;
+
+	schoolClass.className = QString("语文");
+	schoolClass.startTime = QTime(8, 0);
+	schoolClass.stopTime = QTime(8, 40);
+	classList.push_back(schoolClass);
+
+	schoolClass.className = QString("体育");
+	schoolClass.startTime = QTime(8, 50);
+	schoolClass.stopTime = QTime(9, 30);
+	classList.push_back(schoolClass);
+
+	schoolClass.className = QString("数学");
+	schoolClass.startTime = QTime(10, 10);
+	schoolClass.stopTime = QTime(10, 50);
+	classList.push_back(schoolClass);
+
+	schoolClass.className = QString("英语");
+	schoolClass.startTime = QTime(11, 0);
+	schoolClass.stopTime = QTime(11, 40);
+	classList.push_back(schoolClass);
+
+	schoolClass.className = QString("美术");
+	schoolClass.startTime = QTime(13, 10);
+	schoolClass.stopTime = QTime(13, 50);
+	classList.push_back(schoolClass);
+
+	schoolClass.className = QString("劳技");
+	schoolClass.startTime = QTime(14, 0);
+	schoolClass.stopTime = QTime(14, 40);
+	classList.push_back(schoolClass);
+
+	schoolClass.className = QString("地理");
+	schoolClass.startTime = QTime(14, 50);
+	schoolClass.stopTime = QTime(15, 30);
+	classList.push_back(schoolClass);
+
+	defaultSchoolTimetable[Qt::DayOfWeek::Sunday] = classList;;
 	return;
 }
